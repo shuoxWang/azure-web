3cf8c9f62cc2aaaf6a8edd021028866e821b1952
Shuo Wang <shuoxwang@deloitte.ca>
2024-12-12 14:34:52 -0700

    2:34PM - 12/12/2024

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,21 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 
 import "../Sidebar/Sidebar.css";
 
-const Sidebar = ({ collapsed}: {collapsed: boolean}) => {
-
+const Sidebar = ({ collapsed }: { collapsed: boolean }) => {
+    const location = useLocation();
     return (
 
         <div className={`sidebar ${collapsed ? "collapsed" : ""}`} >
             <div className="sidebar-content">
-                <div className="nav-item">
-                    <img src="/query-svgrepo-com.svg" alt="query icon" className="nav-icon" />
-                    <Link to="/query" className="nav-link-custom">Query</Link>
+                <div className={`nav-item ${location.pathname === "/query" ? "active" : ""}`}>
+                    <Link to="/query" className="nav-link-custom">
+                        <img src="/query-svgrepo-com.svg" alt="query icon" className="nav-icon" />
+                        <span>Query</span>
+                    </Link>
                 </div>
-                <div className="nav-item">
-                    <img src="/compass-svgrepo-com.svg" alt="dashboards icon" className="nav-icon" />
-                    <Link to="/dashboards" className="nav-link-custom">Dashboards</Link>
+                <div className={`nav-item ${location.pathname === "/dashboards" ? "active" : ""}`}>
+                    <Link to="/dashboards" className="nav-link-custom">
+                        <img src="/compass-svgrepo-com.svg" alt="dashboards icon" className="nav-icon" />
+                        <span>Dashboards</span>
+                    </Link>
                 </div>
             </div>
         </div>
